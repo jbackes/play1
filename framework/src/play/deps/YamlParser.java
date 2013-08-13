b9e38fb5da451d840abcbade411dd08168363a1e
Julian Backes <julian.backes@peloba.de>
2013-08-13 13:46:11 +0200

    added ebean as core module

--- framework/src/play/deps/YamlParser.java
+++ framework/src/play/deps/YamlParser.java
@@ -125,6 +125,9 @@ public class YamlParser extends AbstractModuleDescriptorParser {
                         if(depName.matches("play\\s+->\\s+secure") || depName.equals("secure")) {
                             depName = "play -> secure " + System.getProperty("play.version");
                         }
+                        if(depName.matches("play\\s+->\\s+ebean") || depName.equals("ebean")) {
+                            depName = "play -> ebean " + System.getProperty("play.version");
+                        }
 
                         Matcher m = Pattern.compile("([^\\s]+)\\s*[-][>]\\s*([^\\s]+)\\s+([^\\s]+)(\\s+[^\\s]+)?.*").matcher(depName);
                         if (!m.matches()) {
